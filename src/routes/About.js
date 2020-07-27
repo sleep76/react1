a3b3323c827627eeb813c433b341d3da84206aa0
sleepz@yonsei.ac.kr <38694755+sleep76@users.noreply.github.com>
2020-07-27 19:14:14 +0900

    1회독 완료 이제 마 개조를 해보자

---
+++ src/routes/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+    return (
+        <div className="about__container">
+            <span>
+                "처음 만들어 본 reactjs 편하긴 하네... "
+            </span>
+            <span>
+                made by sleepz
+            </span>
+        </div>
+    );
+}
+
+export default About;
